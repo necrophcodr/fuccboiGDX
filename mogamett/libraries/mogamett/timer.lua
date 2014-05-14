6e4fd24b371b57773f40b1b7c8df04e4ecc10d87
Felipe <adonaac@gmail.com>
2014-05-14 18:31:06 -0300

    Everything seems to work!

--- mogamett/libraries/mogamett/timer.lua
+++ mogamett/libraries/mogamett/timer.lua
@@ -10,26 +10,37 @@ end
 function timer:update(dt)
     self.timer:update(dt)
 end
-
-function timer:tween(tag, duration, table, tween_table, tween_function, after, ...)
+function timer:tween(tag, duration, table, tween_table, tween_function, after)
+    if type(tag) == 'number' or type(tag) == 'table' then
+        return self.timer:tween(tag, duration, table, tween_table, tween_function)
+    end
     if not self.tags[tag] then
-        self.tags[tag] = self.timer:tween(duration, table, tween_table, tween_function, after, ...)
+        self.tags[tag] = self.timer:tween(duration, table, tween_table, tween_function, after)
     end
 end
 
 function timer:after(tag, duration, func)
+    if type(tag) == 'number' or type(tag) == 'table' then
+        return self.timer:after(tag, duration)
+    end
     if not self.tags[tag] then
         self.tags[tag] = self.timer:after(duration, func)
     end
 end
 
 function timer:every(tag, duration, func, count)
+    if type(tag) == 'number' or type(tag) == 'table' then
+        return self.timer:every(tag, duration, func)
+    end
     if not self.tags[tag] then
         self.tags[tag] = self.timer:every(duration, func, count)
     end 
 end
 
 function timer:during(tag, duration, func, after)
+    if type(tag) == 'number' or type(tag) == 'table' then
+        return self.timer:during(tag, duration, func)
+    end
     if not self.tags[tag] then
         self.tags[tag] = self.timer:during(duration, func, after)
     end
@@ -39,7 +50,7 @@ function timer:cancel(tag)
     if self.tags[tag] then
         self.timer:cancel(self.tags[tag])
         self.tags[tag] = nil
-    end
+    else self.timer:cancel(tag) end
 end
 
 function timer:clear()
